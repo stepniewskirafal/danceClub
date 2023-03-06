39f4036522bbcc1fdd4e1b58c69a9b7077786219
Rafał Stępniewski <vultur12@gmail.com>
2023-03-06 09:24:01 +0100

    Dane testowe

---
+++ src/main/resources/db/testdata/0001_school.sql
@@ -0,0 +1,5 @@
+insert into school (name, address) values ('pellentesque quisque', '3538 Maple Wood Plaza');
+insert into school (name, address) values ('maecenas leo odio condimentum', '40493 Sommers Hill');
+insert into school (name, address) values ('ultrices mattis', '23523 Nova Court');
+insert into school (name, address) values ('nulla ut', '77 Mandrake Drive');
+insert into school (name, address) values ('mauris laoreet', '30 Boyd Terrace');
